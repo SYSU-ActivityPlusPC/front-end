534e3446a936253762eb46c15d9014e2e2aec71c
ZhuLifeng <820917549@qq.com>
2017-07-27 00:14:21 +0800

    working

--- src/pages/admin/children/communityManage/children/detail.vue
+++ src/pages/admin/children/communityManage/children/detail.vue
@@ -1,14 +1,65 @@
 <template>
-<div>
+<div class="detail-wrapper">
+  <div class="item">
+    <label class="label">社团名称</label>
+    <span>SDCS团委</span>
+  </div>
+  <div class="item">
+    <label class="label">账号</label>
+    <span>1234566</span>
+  </div>
+  <div class="item">
+    <label class="label">密码</label>
+    <span>1234566</span>
+  </div>
+  <div class="item">
+    <label class="label">注册时间</label>
+    <span>2015.6.4</span>
+  </div>
+  <div class="item">
+    <label class="label">证明材料</label>
+    <div>
+      <p>活动1</p>
+      <p>活动2</p>
+      <p>活动3</p>
+      <p>活动1</p>
+      <p>活动2</p>
+      <p>活动3</p>
+      <p>活动1</p>
+      <p>活动2</p>
+      <p>活动3</p>
+    </div>
+  </div>
+  <div class="item">
+    <label class="label">已发布活动</label>
+    <div>
+      <p>活动1</p>
+      <p>活动2</p>
+      <p>活动3</p>
+    </div>
+  </div>
 </div>  
 </template>
 
 <script>
 export default {
-
 };
 </script>
 
 <style scoped>
-  
+.detail-wrapper {
+  height: 100%;
+  margin-top: 1.471%;
+}
+
+.item {
+  display: flex;
+  margin: 10px 0;
+}
+
+.label {
+  width: 110px;
+  text-align: right;
+  margin-right: 40px;
+}
 </style>
