2ae4c69365a34a0b8949d0af66f909eb874a1d00
ZhuLifeng <820917549@qq.com>
2017-07-28 20:10:37 +0800

    working

--- src/pages/community/children/activityMange/children/signup.vue
+++ src/pages/community/children/activityMange/children/signup.vue
@@ -1,30 +1,40 @@
 <template>
 <div class="signup-wrapper">
-  <div>
-    <div>序号</div>
-    <div>姓名</div>
-    <div>性别</div>
-    <div>学号</div>
-    <div>学院</div>
-    <div>联系电话</div>
-    <div>操作</div>
+  <div class="button-group">
+    <MyButton class="end-button">结束报名</MyButton>
+    <MyButton>导出名单</MyButton>
   </div>
-  <template v-for="(item, index) in mock">
-    <div :key="index">
-      <div>{{index}}</div>
-      <div>{{item.name}}</div>
-      <div>{{item.gender}}</div>
-      <div>{{item.sid}}</div>
-      <div>{{item.college}}</div>
-      <div>{{item.phone}}</div>
-      <a href="javascript:void(0)">删除</a>
-    </div>   
-  </template>
+  <div class="table">
+    <div class="row">
+      <div class="col col-name col-width-70">序号</div>
+      <div class="col col-name col-flex-1">姓名</div>
+      <div class="col col-name col-width-70">性别</div>
+      <div class="col col-name col-width-100">学号</div>
+      <div class="col col-name col-flex-1">学院</div>
+      <div class="col col-name col-width-150">联系电话</div>
+      <div class="col col-name col-width-70">操作</div>
+    </div>
+    <template v-for="(item, index) in mock">
+      <div :key="index" class="row" :class="{'bg': index % 2 === 0}">
+        <div class="col col-width-70">{{index}}</div>
+        <div class="col col-flex-1">{{item.name}}</div>
+        <div class="col col-width-70">{{item.gender}}</div>
+        <div class="col col-width-100">{{item.sid}}</div>
+        <div class="col col-flex-1">{{item.college}}</div>
+        <div class="col col-width-150">{{item.phone}}</div>
+        <a href="javascript:void(0)" class="col col-width-70 operator">删除</a>
+      </div>   
+    </template>
+  </div>
 </div>  
 </template>
 
 <script>
+import { MyButton } from '@/components';
 export default {
+  components: {
+    MyButton
+  },
   data () {
     return {
       mock: [
@@ -50,7 +60,7 @@ export default {
           phone: 13719177292
         }
       ]
-    }
+    };
   }
 };
 </script>
@@ -58,5 +68,61 @@ export default {
 <style scoped>
 .signup-wrapper {
   width: 68.965%;
-}  
+}
+
+.table {
+  border: 1px solid #cccccc;
+  border-bottom: transparent;
+  border-right: transparent;
+}
+
+.row {
+  display: flex;
+}
+
+.col {
+  border: 1px solid #cccccc;
+  border-left: transparent;
+  border-top: transparent;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+
+.col-flex-1 {
+  flex: 1
+}
+
+.col-width-70 {
+  width: 70px;
+}
+
+.col-width-100 {
+  width: 100px;
+}
+
+.col-width-150 {
+  width: 150px;
+}
+
+.operator {
+  color: #f91c6f;
+}
+
+.col-name {
+  color: #5074d7;
+}
+
+.bg {
+  background-color: #f2f2f2;
+}
+
+.button-group {
+  text-align: right;
+  margin-bottom: 2.25%;
+}
+
+.end-button {
+  margin-right: 3.75%
+}
 </style>
