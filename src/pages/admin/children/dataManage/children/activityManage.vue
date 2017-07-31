54b0844e492e1146f81d171d2431893809337e8f
ZhuLifeng <820917549@qq.com>
2017-07-31 19:14:03 +0800

    youhua

--- src/pages/admin/children/dataManage/children/activityManage.vue
+++ src/pages/admin/children/dataManage/children/activityManage.vue
@@ -6,48 +6,28 @@
         <li :class="{'select': selectCampus === `${item}`}" :key="item" @click="selectCampus=item">{{item}}校区</li>
       </template>
     </ul>
-    <span class="num">共3个</span>
+    <span class="num">共{{mock.length}}个</span>
   </div>
-  <div class="lists">
-    <transition-group name="flip-list">
-      <template v-for="(item, index) in mock">
-        <div class="list-wrapper" :key="item.time">
-          <ListItem @click="open=true"  class="list-item">
-            <div slot="left">
-              <p class="list-title">{{item.title}}</p>
-              <p class="font-small">{{item.time}}提交</p>
-            </div>
-            <div slot="right" class="right">
-              <div class="list-right-item">
-                <p class="font-small">来源</p>
-                <p style="color: #666666;">{{item.src}}</p>
-              </div>
-              <div class="list-right-item">
-                <p class="font-small">活动对象</p>
-                <p style="color: #666666;">{{item.tar}}</p>
-              </div>
-              <div class="list-right-item">
-                <a href="javascript:void(0)" class="up" @click.stop="up(index)">上移</a>
-                <a href="javascript:void(0)" class="down" @click.stop="down(index)">下移</a>
-              </div>
-            </div>
-          </ListItem>
-          <iButton type="text" class="button-remove">
-            <img :src="remove" alt="删除" />
-          </iButton>
-        </div>
-      </template>
-    </transition-group>
-  </div>
+  <transition-group name="flip-list" tag="div" class="lists">
+    <template v-for="(item, index) in mock">
+      <ListItem @click="open=true" 
+                @remove="remove(index)"
+                :key="item.time" 
+                :item="item">
+          <div class="list-right-item" slot="right">
+            <a href="javascript:void(0)" class="up" @click.stop="up(index)">上移</a>
+            <a href="javascript:void(0)" class="down" @click.stop="down(index)">下移</a>
+          </div>
+      </ListItem>
+    </template>
+  </transition-group>
   <Modal :open="open" @close="open=false"/>
 </div>
 </template>
 
 <script>
-import ListItem from '@/components/listItem';
-import iButton from 'iview/src/components/button';
+import ListItem from './children/listItem';
 import Modal from './children/modal';
-import remove from '@/assets/delete';
 
 const mock = [
   {
@@ -79,13 +59,11 @@ const mock = [
 export default {
   components: {
     ListItem,
-    iButton: iButton,
     Modal
   },
   data () {
     return {
       mock,
-      remove,
       open: false,
       selectCampus: '东',
       campus: [
@@ -112,12 +90,23 @@ export default {
         this.mock[index] = temp;
         this.mock = JSON.parse(JSON.stringify(this.mock));
       }
+    },
+    remove (index) {
+      this.mock.splice(index, 1);
     }
   }
 };
 </script>
 
 <style scoped>
+.lists {
+  display: flex;
+  flex-direction: column;
+}
+
+.wrapper {
+  position: relative;
+}
 
 .wrapper-tabs {
   height: 40px;
@@ -151,25 +140,11 @@ li {
 }
 
 
-.list-title {
-  font-size: 16px;
-  color: #666666;
-}
-
 .list-right-item {
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-left: 40px;
-}
-
-.font-small {
-  color: #999999;
-  font-size: 10px;
-}
-
-.right {
-  display: flex;
 }
 
 .up {
@@ -180,18 +155,5 @@ li {
   color: #5074d7;
 }
 
-
-.list-wrapper {
-  display: flex;
-  margin-bottom: 2.85%;
-  position: relative;
-}
-
-.button-remove {
-  position: absolute;
-  right: -60px;
-  top: 50%;
-  transform: translateY(-50%);
-}
 
 </style>
---
+++ src/pages/admin/children/dataManage/children/children/listItem.vue
@@ -0,0 +1,86 @@
+<template>
+<div class="list-wrapper">
+  <ListItem @click="onClick"  class="list-item">
+    <div slot="left">
+      <p class="list-title">{{item.title}}</p>
+      <p class="font-small">{{item.time}}提交</p>
+    </div>
+    <div slot="right" class="right">
+      <div class="list-right-item">
+        <p class="font-small">来源</p>
+        <p style="color: #666666;">{{item.src}}</p>
+      </div>
+      <div class="list-right-item">
+        <p class="font-small">活动对象</p>
+        <p style="color: #666666;">{{item.tar}}</p>
+      </div>
+      <slot name="right"></slot>
+    </div>
+  </ListItem>
+  <iButton type="text" class="button-remove" @click="onRemove">
+    <img :src="remove" alt="删除" />
+  </iButton>
+</div>
+</template>
+
+<script>
+import ListItem from '@/components/listItem';
+import iButton from 'iview/src/components/button';
+import remove from '@/assets/delete';
+export default {
+  props: ['item'],
+  components: {
+    ListItem,
+    iButton
+  },
+  data () {
+    return {
+      remove
+    };
+  },
+  methods: {
+    onClick () {
+      this.$emit('click');
+    },
+    onRemove () {
+      this.$emit('remove');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.list-wrapper {
+  display: flex;
+  margin-bottom: 2.85%;
+  position: relative;
+}
+
+.button-remove {
+  position: absolute;
+  right: -60px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.list-title {
+  font-size: 16px;
+  color: #666666;
+}
+
+.list-right-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 40px;
+}
+
+.font-small {
+  color: #999999;
+  font-size: 10px;
+}
+
+.right {
+  display: flex;
+}
+</style>
