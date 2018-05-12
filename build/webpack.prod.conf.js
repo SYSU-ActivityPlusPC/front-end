bcd54c92c012c23e3bb4172046ad469b33d4d2fe
Tang Xuanzhao <xuanzhaotang@gmail.com>
2018-05-12 20:17:02 +0800

    add prefix to webpack

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -25,6 +25,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    publicPath: "/admin/"
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
